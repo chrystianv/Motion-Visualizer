d0ea6723454a4529b505fefff88163c76fe82ea5
Chrystian Vieyra <chrys.vieyra@gmail.com>
2024-07-13 15:48:23 -0400

    Initial Commit

---
+++ Motion Visualizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motion Visualizer
+//
+//  Created by Chrystian Vieyra on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motion Visualizer/Motion_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motion_VisualizerApp.swift
+//  Motion Visualizer
+//
+//  Created by Chrystian Vieyra on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Motion_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
